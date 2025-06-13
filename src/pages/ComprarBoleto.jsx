e3ecfd51d09b216739aebab70e6f25f45ef316b0
Firecrasht <ricotourspv@gmail.com>
2025-06-13 12:34:14 -0600

    Delete src directory

--- src/pages/ComprarBoleto.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ComprarBoleto = () => <h2>Comprar Boleto</h2>;
-
-export default ComprarBoleto;
--- src/pages/Home.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Home = () => <h1>Bienvenido a Rico Tours</h1>;
-
-export default Home;
